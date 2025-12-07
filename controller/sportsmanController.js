23be3a2d084d20aa86ae9d1ba5477ed1d49f379b
Cyber7711 <sarvareliboyev8@gmail.com>
2025-12-07 22:05:45 +0500

    sendResponse

--- controller/sportsmanController.js
+++ controller/sportsmanController.js
@@ -1,20 +1,34 @@
-const sportsmanService = require("../service/sportsmanService");
-const AppError = require("../utils/appError");
+const catchAsync = require("../middleware/asyncWrapper");
+const sendResponse = require("../middleware/sendResponse");
+const SportsmanService = require("../service/sportsmanService");
 
-const createSportsman = async (req, res, next) => {
-  try {
-    const result = await sportsmanService.createSportsman(req.body);
-    if (!result) {
-      throw new AppError("Sportchi yaratishda xatolik", 401);
-    }
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Sportchi muvaffaqiyatli yaratildi",
-        data: result,
-      });
-  } catch (err) {
-    return next(err);
-  }
-};
+const create = catchAsync(async (req, res) => {
+  const result = await SportsmanService.create(req.body, req.user._id);
+  sendResponse(res, {
+    status: 201,
+    message: "Sportsman muvaffaqiyatli yaratildi",
+    data: result,
+  });
+});
+
+const getAll = catchAsync(async (req, res) => {
+  const result = await SportsmanService.getAll();
+  sendResponse(res, {
+    status: 200,
+    message: "Sportchilar muvaffaqiyatli olindi",
+    data: result,
+  });
+});
+
+const getById = catchAsync(async (req, res) => {
+  const result = await SportsmanService.getById(req.params.id);
+  sendResponse(res, {
+    status: 200,
+    message: "Sportchi muvaffaqiyatli olindi",
+    data: result,
+  });
+});
+// https://www.instagram.com/reel/DPybfIdj_Z3/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ==
+// https://www.instagram.com/reel/DQUDyyADcJV/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ==
+// https://www.instagram.com/reel/DL_caZMI83q/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ==
+// https://www.instagram.com/reel/DRBZbWTiPi3/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ==
